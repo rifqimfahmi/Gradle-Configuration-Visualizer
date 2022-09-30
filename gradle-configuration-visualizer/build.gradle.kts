d6fef26b0fe0b0ac7bec55f632ff992f07ac8f91
rifqi.fahmi <rifqi.fahmi@grabtaxi.com>
2022-09-30 20:56:00 +0800

    update plugin desc

--- gradle-configuration-visualizer/build.gradle.kts
+++ gradle-configuration-visualizer/build.gradle.kts
@@ -2,17 +2,30 @@ plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    id("maven-publish")
+    id("com.gradle.plugin-publish") version "1.0.0"
 }
+
+group = "dev.rifqimfahmi.gcv.plugin"
+version = "1.0.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
 
+pluginBundle {
+    website = "rifqimfahmi.dev"
+    vcsUrl = "https://github.com/rifqimfahmi/Gradle-Configuration-Visualizer"
+    tags = listOf("gradle", "configurations", "classpath", "api", "graph")
+}
+
 gradlePlugin {
     plugins {
-        create("gcvPlugin") {
+        create("gcv") {
             id = "dev.rifqimfahmi.gcv.plugin"
+            displayName = "Gradle Config Visualizer"
+            description = "Visualize Gradle project configurations and identify resolveable configurations"
             implementationClass = "dev.rifqimfahmi.gcv.plugin.GradleConfigVisualizerPlugin"
         }
     }
